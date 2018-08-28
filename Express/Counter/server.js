53e94111b857820fdab6a1369acf64e398aea0dd
Warren <w.e.hellman@gmail.com>
2018-08-28 14:56:06 -0700

    Still updating new repository

---
+++ Express/Counter/server.js
@@ -0,0 +1,43 @@
+// require express
+var express = require("express");
+// path module -- try to figure out where and why we use this
+var path = require("path");
+// create the express app
+var session = require("express-session");
+var app = express();
+
+app.use(session({
+  secret: 'catwrestler',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 60000 }
+}))
+
+var bodyParser = require('body-parser');
+// use it!
+app.use(bodyParser.urlencoded({ extended: true }));
+// static content
+app.use(express.static(path.join(__dirname, "./static")));
+// setting up ejs and our views folder
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
+// root route to render the index.ejs view
+app.get('/', function(req, res) {
+  res.render("index", { count: addOne(req)} );
+});
+
+function addOne(req) {
+  if (!req.session.count){
+    req.session.count = 1;
+  }
+  else {req.session.count +=1}
+  console.log('session is at '+req.session.count)
+  return req.session.count;
+}
+
+
+// tell the express app to listen on port 8000
+app.listen(8000, function() {
+  console.log("listening on port 8000");
+});
+
