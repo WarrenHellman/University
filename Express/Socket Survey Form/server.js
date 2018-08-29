fa3e14e8b2ff94c3452a4835fd11b900aa59895d
Warren <w.e.hellman@gmail.com>
2018-08-29 15:24:11 -0700

    Socket Survey Form

---
+++ Express/Socket Survey Form/server.js
@@ -0,0 +1,30 @@
+// require express
+var express = require("express");
+// path module -- try to figure out where and why we use this
+var path = require("path");
+// create the express app
+var session = require("express-session");
+var app = express();
+
+app.use(session({
+  secret: 'catwrestler',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 60000 }
+}))
+
+var bodyParser = require('body-parser');
+// use it!
+app.use(bodyParser.urlencoded({ extended: true }));
+// static content
+app.use(express.static(path.join(__dirname, "./static")));
+// setting up ejs and our views folder
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
+
+require('./routes/route.js')(app);
+// tell the express app to listen on port 8000
+app.listen(8000, function() {
+  console.log("listening on port 8000");
+});
+
