d8d99c0e8dc874d1c65c3f88eee32867e1a801eb
Warren <w.e.hellman@gmail.com>
2018-08-30 08:28:52 -0700

    updates

--- Express/Socket Survey Form/routes/route.js
+++ Express/Socket Survey Form/routes/route.js
@@ -3,5 +3,8 @@ module.exports = function Route(app){
   app.get('/', function(req, res) {
     res.render("index");
   });
+  app.post('/submit', function(req, res) {
+    res.redirect('/')
+  })
 
 };
--- Express/Socket Survey Form/server.js
+++ Express/Socket Survey Form/server.js
@@ -23,8 +23,17 @@ app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
 
 require('./routes/route.js')(app);
-// tell the express app to listen on port 8000
-app.listen(8000, function() {
-  console.log("listening on port 8000");
+
+const server = app.listen(8000)
+const io = require('socket.io')(server);
+var counter = 0;
+    
+io.on('connection', function (socket) { //2
+  
+  socket.emit('greeting', { msg: 'Greetings, from server Node, brought to you by Sockets! -Server' }); //3
+  socket.on('thankyou', function (data) { //7
+    console.log(data.msg); //8 (note: this log will be on your server's terminal)
+  });
+    
 });
 
