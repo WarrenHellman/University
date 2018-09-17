5174d7bc0d50fde7ba7cd0c4341f7ffd48d2c0dc
Warren <w.e.hellman@gmail.com>
2018-09-17 12:28:28 -0700

    Finished Quote log

--- Express/quoting_dojo/server.js
+++ Express/quoting_dojo/server.js
@@ -4,26 +4,26 @@ var server = require('http').Server(app);
 var path = require("path");
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose')
-var session = require("express-session");
+// var session = require("express-session");
 
-app.use(session({
-  secret: 'catwrestler',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { maxAge: 60000 }
-}))
+// app.use(session({
+//   secret: 'catwrestler',
+//   resave: false,
+//   saveUninitialized: true,
+//   cookie: { maxAge: 60000 }
+// }))
 
 // This is how we connect to the mongodb database using mongoose -- "basic_mongoose" is the name of
 //   our db in mongodb -- this should match the name of the db you are going to use for your project.
-mongoose.connect('mongodb://localhost/basic_mongoose');
+mongoose.connect('mongodb://localhost/quote_log');
 
-var UserSchema = new mongoose.Schema({
+var Quote = new mongoose.Schema({
   name: String,
-  age: Number
- })
- mongoose.model('User', UserSchema); // We are setting this Schema in our Models as 'User'
- var User = mongoose.model('User') // We are retrieving this Schema from our Models, named 'User'
-
+  quote: String,
+  postDate: {type: Date, default: Date.now}
+})
+mongoose.model('Quoter', Quote); // We are setting this Schema in our Models as 'User'
+var Quoter = mongoose.model('Quoter') // We are retrieving this Schema from our Models, named 'User'
 // use it!
 app.use(bodyParser.urlencoded({ extended: true }));
 // static content
@@ -31,20 +31,30 @@ app.use(express.static(path.join(__dirname, "./static")));
 // setting up ejs and our views folder
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
+
 app.get('/', function(req, res) {
-  // User.find({}, function(err, users){
-    
-  // })
   res.render("index");
-  
 });
 app.get('/quotes', function(req, res){
-  res.render("quotes");
+  Quoter.find({}, function(err, quotes) {
+    if(err){console.log(err);}
+    res.render("quotes", {quotes:quotes});
+  });
+  
+  
 });
 app.post('/quotes', function(req, res){
   console.log("POST DATA: ", req.body);
-
-  res.redirect("/quotes");
+  let quoter= new Quoter(req.body);
+  quoter.save(function(err){
+    if (err) {
+      console.log("There is an error")
+      res.redirect('/');
+    }
+    else {
+      res.redirect('/quotes');
+    }
+  })
 });
 
 server.listen(80);
