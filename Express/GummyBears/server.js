be36aa7df260c5cb0c78cb7d364e7ce0b856750b
Warren <w.e.hellman@gmail.com>
2018-09-19 14:51:45 -0700

    Commented server page and updated some of the pages

--- Express/GummyBears/server.js
+++ Express/GummyBears/server.js
@@ -5,37 +5,45 @@ var path = require("path");
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose')
 
+//Database Mongoose will use
 mongoose.connect('mongodb://localhost/bear_house');
 
-var Bear = new mongoose.Schema({
+// The schema serves as our constructor 
+const Bear = new mongoose.Schema({
   name: String,
   age: String,
   about: String
 })
-mongoose.model('Member', Bear); // We are setting this Schema in our Models as 'User'
-var Member = mongoose.model('Member') // We are retrieving this Schema from our Models, named 'User'
-// use it!
+
+mongoose.model('Member', Bear); // We are setting this Schema in our Models as 'Member'
+const Member = mongoose.model('Member') // We are retrieving this Schema from our Models, named 'User'
+// use it! allows to pull info from forms (req.body)
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// static content
+// static content- for all css and photos
 app.use(express.static(path.join(__dirname, "./static")));
 
 // setting up ejs and our views folder
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
 
+//instructions for root
 app.get('/', function(req, res) {
+  //looks up all members in our bear_house database
   Member.find({}, function(err, members){
     if (err) {
       console.log(err)
     }
+    // creates and returns an object 'members' with the returned database members as a value
     res.render("index", {members: members});
   })
-  
 });
+
 app.get('/bears/new', function(req, res){
   res.render("new");
 });
+
+// creates a new member with the info from the form, saves it and goes back to root
 app.post('/bears', function(req, res) {
   let newBear = new Member(req.body);
   newBear.save(function(err){
@@ -46,29 +54,35 @@ app.post('/bears', function(req, res) {
     res.redirect('/');
   })
 });
+
+//The :id stores as a params variable the unique id we are passing through the url.
 app.post('/bears/:id', function(req, res){
   console.log(req.params.id)
+  // Queries the database for the unique id, passes all of the form info to completely update the database document
   Member.update({_id: req.params.id}, req.body, function(err, result){
     if(err) {console.log(err)}
     res.redirect("/")
   });
 });
 
+//Again, the url contains a parameter variable id
 app.get('/bears/edit/:id', function(req, res){
-
   Member.find({_id: req.params.id}, function(err, response){
     if (err){console.log(err)}
+    // passes the info from our response (which is expecting a single user, first index of the array). 
     res.render('edit', {bear: response[0]})
   })
 })
-app.get('/bears/:id', function(req, res){
 
+//shows info for each bear member. 
+app.get('/bears/:id', function(req, res){
   Member.find({_id: req.params.id}, function(err, response){
     if (err){console.log(err)}
     res.render('profile', {bear: response[0]})
   })
 })
 
+//deletes a user
 app.post('/bears/delete/:id', function(req, res) {
   Member.remove({_id: req.params.id}, function (err, response){
     if (err) {console.log(err)}
