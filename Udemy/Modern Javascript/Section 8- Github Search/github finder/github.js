e1a2d068e17485edd6e76db69504c81494983785
Warren Hellman <w.e.hellman@gmail.com>
2018-09-21 11:36:11 -0700

    Modern JS Github Project

--- Udemy/Modern Javascript/Section 8- Github Search/github finder/github.js
+++ Udemy/Modern Javascript/Section 8- Github Search/github finder/github.js
@@ -2,15 +2,21 @@ class GitHub {
   constructor() {
     this.client_id = '93aff38a20732a7cd7c0';
     this.client_secret = '95f422b4b8bd29583cc8ec65dc795fde20f252ce';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
 
   async getUser(user) {
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
 
     return {
-      profile
+      profile,
+      repos
     }
   }
 }
