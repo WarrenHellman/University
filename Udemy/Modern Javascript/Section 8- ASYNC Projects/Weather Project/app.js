97e2f687a09765430f715941c26ce26d8b4108be
Warren Hellman <w.e.hellman@gmail.com>
2018-09-24 16:00:13 -0700

    Weather JS app finished

--- Udemy/Modern Javascript/Section 8- ASYNC Projects/Weather Project/app.js
+++ Udemy/Modern Javascript/Section 8- ASYNC Projects/Weather Project/app.js
@@ -1,9 +1,28 @@
+//Init storage
+const storage = new Storage();
+// Get stored location data
+const weatherLocation = storage.getLocationData();
 //Init weather Obj
-const weather = new Weather('Seattle');
+const weather = new Weather(weatherLocation.city);
 
 //Init UI
-
 const ui = new UI();
+
+//Change location event
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+  const city = document.getElementById('city').value;
+  
+  weather.changeLocation(city)
+
+  //set location in local storage
+  storage.setLocationData(city)
+
+  //get and display weather
+  getWeather();
+
+  // Close Modal
+  $('#locModal').modal('hide');
+})
 
 // Get weather on DOM load:
 document.addEventListener('DOMContentLoaded', getWeather);
--- Udemy/Modern Javascript/Section 8- ASYNC Projects/Weather Project/ui.js
+++ Udemy/Modern Javascript/Section 8- ASYNC Projects/Weather Project/ui.js
@@ -7,19 +7,18 @@ class UI {
     this.icon = document.getElementById('w-icon');
     this.humidity = document.getElementById('w-humidity');
     this.windSpeed = document.getElementById('w-windSpeed');
-    this.sunrise = document.getElementById('w-sunrise');
-    this.sunset = document.getElementById('w-sunset');
+    this.clouds = document.getElementById('w-clouds');
+    this.high = document.getElementById('w-high');
   }
 
   paint(weather) {
     this.location.textContent = weather.name;
     this.desc.textContent = weather.weather[0].main;
-    this.string.textContent = weather.main.temp;
-    //this.icon.setAttribute('src', 'url')
-    this.humidity.textContent = `Relative Humidity: ${weather.main.humidity}`
-    this.windSpeed.textContent = `Wind Speed: ${weather.wind.speed}`
-    this.sunrise.textContent = `Sunrise at: ${weather.sys.sunrise}`
-    this.sunset.textContent = `Sunset at: ${weather.sys.sunset}`
-    console.log(weather);
+    this.string.textContent = `${Math.round(weather.main.temp*9/5 - 459.67)}°F`;
+    this.icon.setAttribute('src', `https://openweathermap.org/img/w/${weather.weather[0].icon}.png`)
+    this.humidity.textContent = `Relative Humidity: ${weather.main.humidity}%`
+    this.windSpeed.textContent = `Wind Speed: ${Math.round(weather.wind.speed*2.2)} mph`
+    this.clouds.textContent = `Percent Cloudy: ${weather.clouds.all}%`
+    this.high.textContent = `Today's high: ${Math.round(weather.main.temp_max*9/5-459.97)}°F`
   }
 }
