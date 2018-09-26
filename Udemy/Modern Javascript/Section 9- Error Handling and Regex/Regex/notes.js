cb32889d748f1877d7b3b05e03024ed32313bda3
Warren Hellman <w.e.hellman@gmail.com>
2018-09-26 10:20:38 -0700

    Bootstrap Validation

--- Udemy/Modern Javascript/Section 9- Error Handling and Regex/Regex/notes.js
+++ Udemy/Modern Javascript/Section 9- Error Handling and Regex/Regex/notes.js
@@ -68,6 +68,35 @@ re = /he..o/i     // the period is a wild card for one symbol once
 
 re = /h*llo/i // asterisk matches and character 0 or more times, can be empty
 re = /gre?a?y/i //  Means optional, used for ambiguous spellings like gray/grey, matches gry too
+re = /[A-Z]ray/  // Matches any uppercase letter
+re = /[a-z]ray/  // Matches any lowercase letter
+re = /[A-Za-z]ray/  // Matches any letter 
+re = /[0-9]ray/  // Matches any number
+
+// Braces {} - Quantifiers
+
+re = /Hel{2}o/  // Matches looks for 2 'L's, must occur {x} amount of times
+re = /Hel{2,4}0/  // Gives a range allowing for between 2-4 'L's
+re = /Hel{2,}0/  // must occur at least {m} times, in this case at least 2
+
+// Parentheses () - Grouping
+re = /([0-9]x{3}){3}/  // Looks for a number and up to 3 'x's, 3 times
+
+// Shorthand Character Classes
+
+re = /\w/;    // word character - alphanumeric or _
+re = /\w+/;    // the plus equals 1 or more character
+re = /\W/;      // non-word characters for capital W. 
+re = /\d/;      // Matches any digit
+re = /\d+/;      // Matches all digit
+re = /\D/;      // Matches any non-digit
+re = /\s/;      // Matches any white space character
+re = /\S/;      // Matches any non-white space character
+re = /Hell\b/i   // Word Boundary, looks for a solo word so it will only match Hell, not Hello. 
+
+//Assertions
+re = /x(?=y)/;  // Matches 'x' only! if followed by 'y', sort of a conditional
+re = /x(?!y)/;  // Matches 'x' only if not followed by 'y', sort of a conditional
 
 // String to match
 re = /gre?a?y\?/i // the forward slash is an escape character so that it will search for the question mark in 'gray?'
