823ae6044a1d76a2719fc8854f6e693288daed01
Warren Hellman <w.e.hellman@gmail.com>
2018-10-01 14:09:55 -0700

    React Udemy Course Set Up

--- Express/Message Board/server.js
+++ Express/Message Board/server.js
@@ -9,12 +9,31 @@ var mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/message_board');
 
 // The schema serves as our constructor 
-const Poster = new mongoose.Schema({
-  name: String
+const CommentSchema = new mongoose.Schema({
+  comment: {type: String, required: [true, 'You must include a comment']}
+});
+
+const MessageSchema = new mongoose.Schema({
+  message: {type: String, required: [true, 'You must submit a post'], minlength: [10, "You message must be more that 10 characters"]},
+  userID: {type: String}
+  comments: [CommentSchema]
+});
+
+const UserSchema = new mongoose.Schema({
+  name: {type: String, required: [true, 'A name is required']},
+  messages: [MessageSchema],
+  comments: [CommentSchema]
 })
 
-// mongoose.model('Member', Bear); // We are setting this Schema in our Models as 'Member'
-const Poster = mongoose.model('Poster') // We are retrieving this Schema from our Models, named 'Poster'
+
+mongoose.model('User', UserSchema); // We are setting this Schema in our Models as 'User'
+const User = mongoose.model('User') // We are retrieving this Schema from our Models, named 'User'
+
+mongoose.model('Message', MessageSchema); 
+const Message = mongoose.model('Message') 
+
+mongoose.model('Comment', CommentSchema); 
+const Post = mongoose.model('Comment') 
 
 // use it! allows to pull info from forms (req.body)
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -28,58 +47,23 @@ app.set('view engine', 'ejs');
 
 //instructions for root
 app.get('/', function(req, res) {
+  
   res.render("index");
 });
 
-app.get('/bears/new', function(req, res){
-  res.render("new");
-});
-
-// creates a new member with the info from the form, saves it and goes back to root
-app.post('/bears', function(req, res) {
-  let newBear = new Member(req.body);
-  newBear.save(function(err){
-    if (err){
-      console.log(err);
-      res.redirect("/bears/new");
-    }
-    res.redirect('/');
+app.post('/message', function(req, res) {
+  let newUser = new User()
+  newUser.name = req.body.name
+  let newMessage = new Message()
+  newMessage.message = req.body.message
+  
+  newUser.save(function(err) {
+    if (err) {console.log(err);}
   })
-});
-
-//The :id stores as a params variable the unique id we are passing through the url.
-app.post('/bears/:id', function(req, res){
-  console.log(req.params.id)
-  // Queries the database for the unique id, passes all of the form info to completely update the database document
-  Member.update({_id: req.params.id}, req.body, function(err, result){
-    if(err) {console.log(err)}
-    res.redirect("/")
-  });
-});
-
-//Again, the url contains a parameter variable id
-app.get('/bears/edit/:id', function(req, res){
-  Member.find({_id: req.params.id}, function(err, response){
-    if (err){console.log(err)}
-    // passes the info from our response (which is expecting a single user, first index of the array). 
-    res.render('edit', {bear: response[0]})
+  newMessage.save(function(err) {
+    if (err) {console.log(err);}
   })
-})
-
-//shows info for each bear member. 
-app.get('/bears/:id', function(req, res){
-  Member.find({_id: req.params.id}, function(err, response){
-    if (err){console.log(err)}
-    res.render('profile', {bear: response[0]})
-  })
-})
-
-//deletes a user
-app.post('/bears/delete/:id', function(req, res) {
-  Member.remove({_id: req.params.id}, function (err, response){
-    if (err) {console.log(err)}
-    res.redirect('/')
-  })
+  res.redirect('/')
 })
 
 
---
+++ Udemy/React 16/Section 2- ES6 Review/es6.js
@@ -0,0 +1,23 @@
+/////////////////////////////////
+///////// Classes ///////////////
+/////////////////////////////////
+
+class Human {
+  constructor() {
+    this.gender = 'male'
+  }
+
+  printGender() {
+    console.log(this.gender);
+  }
+}
+
+class Person extends Human {
+  constructor() {
+    super();
+    this.name = 'Warren'
+  }
+  printMyName() {
+    console.log(this.name);
+  }
+}
