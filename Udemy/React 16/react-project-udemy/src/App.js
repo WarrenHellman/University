ef68d98a70c7c02644c8da62444e80181f6e3fbc
Warren Hellman <w.e.hellman@gmail.com>
2018-10-02 11:18:20 -0700

    Finished Section 3 of Udemy React

--- Udemy/React 16/react-project-udemy/src/App.js
+++ Udemy/React 16/react-project-udemy/src/App.js
@@ -1,13 +1,61 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
+import './Person/Person.css'
 
 class App extends Component {
+  // State is a property of the Component class (available here by extending it through App). It is only available in class based components It is an object that we can access in our login through this.state.key). The great thing about state is when it changes, it will re-render the component.
+  state = {
+    persons: [
+      {name: 'Warren', age: 34},
+      {name: 'Erin', age: 39},
+      {name: 'Sharps', age: 13}
+    ]
+  }
+  switchNameHandler = (newName) => {
+    //console.log('clicked')
+    //Don't do this: this.state.persons[1].name = "Erin the Terrific"
+    this.setState({persons: [
+      {name: 'Warren', age: 34},
+      {name: 'Erin the Terrific', age: 39},
+      {name: newName, age: 1}
+    ]})
+  }
+
+  nameChangeHandler = (event) => {
+    this.setState({persons: [
+      {name: event.target.value, age: 34},
+      {name: 'Erin', age: 39},
+      {name: 'Sharpizoid', age: 13}
+    ]})
+  }
   render() {
+    // adding inline style
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px;',
+      cursor: 'pointer'
+    }
     return (
       <div className="App">
         <h1>Hey there handsome!</h1>
-        <Person/>
+        <button 
+        style={style}
+        onClick={() => this.switchNameHandler('Sick Anon')}>Switch Name</button>
+        {/* props can be hard coded or accessed through the state property */}
+        <Person 
+          name={this.state.persons[0].name} 
+          age="28"
+          changed ={this.nameChangeHandler}/>
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Sharpie-brah')}>What it do</Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age}/>
       </div>
     );
 
--- Udemy/React 16/react-project-udemy/src/Person/Person.js
+++ Udemy/React 16/react-project-udemy/src/Person/Person.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
-const person = () => {
-  return <p>I'm a Person!</p>
+const person = (props) => {
+  return (
+    <div className="Person">
+      {/* below we are passing a click function as a prop from our App class where the switchNameHandler method was defined. This is the preferred way to pass methods from our class without give access to the state. This is very important because our functional components cannot create methods and are state-less */}
+      <p onClick={props.click}>I'm {props.name} and I'm {props.age} years old!</p>
+      <p>{props.children}</p>
+      <input type='text' onChange={props.changed} value={props.name}/>
+    </div>
+  )
 }
 
 export default person;
