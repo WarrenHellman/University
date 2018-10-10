03c807c0c7f66a4593c459c4c0b6f527edba9dff
Warren Hellman <w.e.hellman@gmail.com>
2018-10-10 16:28:49 -0700

    Wrongly built Router

--- Contract Work/RTTDW React/rttdw-react/src/CardBody/CardBody.js
+++ Contract Work/RTTDW React/rttdw-react/src/CardBody/CardBody.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './CardBody.css';
 
+
 const CardBody = () => {
   return(
     <div className='CardBody'>
-
+      <div className='breadCrumbs'>
+        <p>Home - Main - Getting Started</p>
+      </div>
+      <div className='pageCard'>
+        <div className='pageBody'>
+          <p>Hellow foold</p>
+        </div>
+      </div>
     </div>
   )
 }
--- Contract Work/RTTDW React/rttdw-react/src/Sidebar/Sidebar.js
+++ Contract Work/RTTDW React/rttdw-react/src/Sidebar/Sidebar.js
@@ -1,23 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './Sidebar.css'
 
 
 const Sidebar = () => {
-  const titles = [[['Who We Are'],[]], [['Getting Started'],['Get Connected', 'Prerequisites', 'Get the Project', 'Contributing', 'Merge Conflicts','Back End Prereqs', 'Deploy Back End']]]
+  const titles = [[['Who We Are'],[], ["'/'"]], [['Getting Started'],['Get Connected', 'Prerequisites', 'Get the Project', 'Contributing', 'Merge Conflicts','Back End Prereqs', 'Deploy Back End']['/gettingStarted']]]
   let navbar = titles.map(item=>{
     let subtitles=''
     if (item[1]){
       subtitles = item[1].map(sub=>{
         return(
-        <h3 className='subLinkCategory'><i class="far fa-square"></i>{sub}</h3>
+        <h3 className='subLinkCategory'><i className="far fa-square"></i>{sub}</h3>
         )
       })
     }
     return (
-      <section>
-        <h3 className='mainLinkCategory'><i class="fas fa-square"></i>{item[0]}</h3>
-        {subtitles}
-      </section>
+      <Router>
+        <section>
+        <Link to='/'><h3 className='mainLinkCategory'><i className="fas fa-square"></i>{item[0]}</h3></Link>
+          {subtitles}
+          <Route exact path ='/' component ={Home}/>
+        </section>
+      </Router>
     )
   })
   return(
@@ -26,5 +30,11 @@ const Sidebar = () => {
     </div>
   )
 }
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
 
 export default Sidebar;
