5488a90451cb0e9eec12e0853487c25715ac276a
Warren Hellman <w.e.hellman@gmail.com>
2018-10-10 12:20:55 -0700

    Format Sidebar

--- Contract Work/RTTDW React/rttdw-react/src/Sidebar/Sidebar.js
+++ Contract Work/RTTDW React/rttdw-react/src/Sidebar/Sidebar.js
@@ -3,19 +3,19 @@ import './Sidebar.css'
 
 
 const Sidebar = () => {
-  const titles = [[['Who We Are'],['Sub1', 'Sub2']], [['Getting Started']]]
+  const titles = [[['Who We Are'],[]], [['Getting Started'],['Get Connected', 'Prerequisites', 'Get the Project', 'Contributing', 'Merge Conflicts','Back End Prereqs', 'Deploy Back End']]]
   let navbar = titles.map(item=>{
     let subtitles=''
     if (item[1]){
       subtitles = item[1].map(sub=>{
         return(
-        <h3>{sub}</h3>
+        <h3 className='subLinkCategory'><i class="far fa-square"></i>{sub}</h3>
         )
       })
     }
     return (
       <section>
-        <h2>{item[0]}</h2>
+        <h3 className='mainLinkCategory'><i class="fas fa-square"></i>{item[0]}</h3>
         {subtitles}
       </section>
     )
