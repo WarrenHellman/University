f326a41d14a3e58bfbe3d90de30b616e82c8822f
Warren Hellman <w.e.hellman@gmail.com>
2018-10-12 10:19:41 -0700

    Ejected!

--- Udemy/React 16/Section 3- Getting Started and Syntax/react-project-udemy/src/App.js
+++ Udemy/React 16/Section 3- Getting Started and Syntax/react-project-udemy/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 import './Person/Person.css'
 
@@ -60,15 +60,16 @@ class App extends Component {
 
   render() {
     // adding inline style
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer',
-    }
+    // const style = {
+    //   backgroundColor: 'green',
+    //   color: 'white',
+    //   font: 'inherit',
+    //   border: '1px solid blue',
+    //   padding: '8px',
+    //   cursor: 'pointer',
+    // }
     let persons= null;
+    let btnClass='';
     if (this.state.showPersons){
       persons = (
         <div>
@@ -84,26 +85,26 @@ class App extends Component {
           })}
         </div>
       );
-      style.backgroundColor = 'red';
-      
+      // style.backgroundColor = 'red';
+      btnClass = classes.Red;
     };
 
-    let classes = [];
+    let assignedClasses = [];
     if (this.state.persons.length<=2){
-      classes.push('red');
+      assignedClasses.push(classes.red);
     }
     if (this.state.persons.length<=1){
-      classes.push('bold')
+      assignedClasses.push(classes.bold)
     }
   
 
     return (
 
-      <div className="App">
+      <div className={classes.App}>
         <h1>Hey there handsome!</h1>
-        <p className={classes.join(' ')}>I'm dynamically styled text!</p>
+        <p className={assignedClasses.join(' ')}>I'm dynamically styled text!</p>
         <button 
-        style={style}
+        
         onClick={() => this.togglePersonsHandler()}>Toggle Persons</button>
 
         {/* If persons !null, persons will render here */}
--- Udemy/React 16/Section 3- Getting Started and Syntax/react-project-udemy/src/Person/Person.js
+++ Udemy/React 16/Section 3- Getting Started and Syntax/react-project-udemy/src/Person/Person.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import classes from './Person.css'
 
 const person = (props) => {
   
   return (
-    <div className="Person">
+    <div className={classes.Person}>
       {/* below we are passing a click function as a prop from our App class where the switchNameHandler method was defined. This is the preferred way to pass methods from our class without give access to the state. This is very important because our functional components cannot create methods and are state-less */}
       <p onClick={props.click}>I'm {props.name} and I'm {props.age} years old!</p>
       <p>{props.children}</p>
