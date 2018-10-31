7406af5c4fa83a2552e97c750f6ef4d58058f17c
Warren Hellman <w.e.hellman@gmail.com>
2018-10-31 11:10:52 -0700

    PNW Nav Bar

--- Contract Work/PNW React/pnw_react/src/Header/Header.js
+++ Contract Work/PNW React/pnw_react/src/Header/Header.js
@@ -5,7 +5,21 @@ class Header extends Component {
   render() {
     return (
       <div className="Header">
-        
+        <header>
+          <nav>
+              <div className="flex-container main-nav">
+                  <ul className="main-nav-1">
+                      <li><a href="#about">ABOUT</a></li>
+                      <li><a href="#photos">PHOTOS</a></li>
+                  </ul>
+                  <img src={require("../images/Sticker.png")} alt="" className="logo"></img>
+                  <ul className="main-nav-2">
+                      <li><a href="#contact">CONTACT US</a></li>
+                      <li><a href="#store">AVAILABILITY</a></li>
+                  </ul>
+              </div>
+          </nav>
+        </header>
       </div>
     );
   }
