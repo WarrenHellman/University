3096f988bc3180dd865c3053f6fa18a60eee6970
Warren Hellman <w.e.hellman@gmail.com>
2018-11-09 10:32:12 -0800

    dafs

--- Contract Work/PNW React/pnw_react/src/Slideshow/Slideshow.js
+++ Contract Work/PNW React/pnw_react/src/Slideshow/Slideshow.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
+// import { render } from 'react-dom';
+import Carousel from 'react-image-carousel';
 import './Slideshow.css';
 
+
+let images = [
+  '../images/warren-temp.jpg',
+  '../images/toshi-temp.jpeg'
+]
 class Slideshow extends Component {
 
   render(){
     return(
       <div classname='Slideshow'>
+        <div className="my-carousel">
+            <Carousel 
+              images={images} 
+              thumb={true}
+              loop={true}
+              autoplay={3000}/>
+        </div>
       </div>
     )
   }
