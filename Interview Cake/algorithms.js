9d39c665dfa51d15de79fd94af72e0ccdf932906
Warren Hellman <w.e.hellman@gmail.com>
2018-11-14 11:43:46 -0800

    Finished Merged Meetings Algorithm from Interview Cake

--- Interview Cake/algorithms.js
+++ Interview Cake/algorithms.js
@@ -1,8 +1,37 @@
-function mergeRanges(schedules) {
-  let newRange = [schedules[0]];
-  schedules.forEach(meeting => {
-    
+function mergeRanges(meetings) {
+  if (meetings.length===0){
+    return 'No meetings in range';
+  }
+  if (meetings.length===1){
+    return meetings;
+  }
+  
+  // Make deep copy
+  const meetingsCopy = JSON.parse(JSON.stringify(meetings));
+  // sort by start time
+  const sortedMeetings = meetingsCopy.sort((a,b)=>{
+    return a.startTime - b.startTime;
   });
+  // Add first meeting to the array we want to return
+  let mergedMeetings=[sortedMeetings[0]];
+  // Used to keep track of our comparison index in the return array
+  let mergedCounter=0
+  // Iterates through sorted meetings
+  for (let i=1; i<sortedMeetings.length; i++){
+    let currentMeeting= sortedMeetings[i];
+    let pastMeeting= mergedMeetings[mergedCounter]
+    //Checks if the current meeting starts before the last meeting's end time and ends after last meeting's end time. If so, it adjusts the last meeting's end time to reflect. 
+    if (currentMeeting.startTime<=pastMeeting.endTime&& currentMeeting.endTime>pastMeeting.endTime){
+      pastMeeting.endTime = currentMeeting.endTime;
+    }
+    // If the current meeting starts after the last meeting's end time, it adds a new entry in teh merged meeting array and adjusts the index for future comparison
+    if (currentMeeting.startTime>pastMeeting.endTime){
+      mergedCounter++;
+      mergedMeetings[mergedCounter]=currentMeeting;
+    }
+  }
+  //Spits out the merged meeting calendar
+  return mergedMeetings;
 }
 
 
