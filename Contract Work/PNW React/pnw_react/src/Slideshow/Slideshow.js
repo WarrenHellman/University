764698c07af5f0dbd5cd9e8706293b3675867aee
Warren Hellman <w.e.hellman@gmail.com>
2019-01-07 13:29:58 -0800

    Have a carousel now

--- Contract Work/PNW React/pnw_react/src/Slideshow/Slideshow.js
+++ Contract Work/PNW React/pnw_react/src/Slideshow/Slideshow.js
@@ -1,22 +1,31 @@
 import React, { Component } from 'react';
+// import ReactDOM from 'react-dom';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
-import './Slideshow.css';
 
 
-let images = [
-  '../images/warren-temp.jpg',
-  '../images/toshi-temp.jpeg'
-]
 class Slideshow extends Component {
-
-  render(){
-    return(
-      <div classname='Slideshow'>
-        <div className="my-carousel">
+  render() {
+    return (
+      <Carousel>
+        <div>
+          <img src={require('../images/toshi-temp.jpeg') } alt="Warren" />
+          <p className="legend">Legend 1</p>
         </div>
-      </div>
+        <div>
+          <img src={require('../images/warren-temp.jpg') } alt="Toshi"/>
+          <p className="legend">Legend 2</p>
+        </div>
+        <div>
+          <img src={require('../images/Sticker.png') } alt="Toshi" />
+          <p className="legend">Legend 3</p>
+        </div>
+      </Carousel>
     )
   }
 }
+
 
 export default Slideshow;
