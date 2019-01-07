5a40a19338d89e7e9592cc6679b1cc48aba04a1e
Warren Hellman <w.e.hellman@gmail.com>
2019-01-07 14:49:35 -0800

    Heigh fixed for slideshow

--- Contract Work/PNW React/pnw_react/src/Slideshow/Slideshow.js
+++ Contract Work/PNW React/pnw_react/src/Slideshow/Slideshow.js
@@ -3,26 +3,30 @@ import React, { Component } from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import './Slideshow.css';
 
 
 
 class Slideshow extends Component {
   render() {
     return (
-      <Carousel>
-        <div>
-          <img src={require('../images/toshi-temp.jpeg') } alt="Warren" />
-          <p className="legend">Legend 1</p>
-        </div>
-        <div>
-          <img src={require('../images/warren-temp.jpg') } alt="Toshi"/>
-          <p className="legend">Legend 2</p>
-        </div>
-        <div>
-          <img src={require('../images/Sticker.png') } alt="Toshi" />
-          <p className="legend">Legend 3</p>
-        </div>
-      </Carousel>
+      <div className="carouselContainer">
+        <Carousel
+          autoPlay>
+          <div className="photoHeight">
+            <img src={require('../images/toshi-temp.jpeg') } alt="Warren"/>
+            <p className="legend">Legend 1</p>
+          </div>
+          <div className="photoHeight">
+            <img src={require('../images/warren-temp.jpg') } alt="Toshi"/>
+            <p className="legend">Legend 2</p>
+          </div>
+          <div className="photoHeight">
+            <img src={require('../images/Sticker.png') } alt="Toshi" />
+            <p className="legend">Legend 3</p>
+          </div>
+        </Carousel>
+      </div>
     )
   }
 }
